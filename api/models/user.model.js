2e82b97d2a69852bbd1be26e402904836a8ce389
patelkrashna13 <P@tel001>
2025-02-20 09:49:47 +0530

    User Model is Changed.

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -6,7 +6,7 @@ const userSchema= new mongoose.Schema(
             type:String,
             required:true,
             unique:true,
-        },
+        },//User Model
         email:{
             type:String,
             required:true,
